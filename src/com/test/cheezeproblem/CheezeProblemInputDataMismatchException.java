4f9c6beb86693d523396e8e8e591d58fb98047d7
Barandeep Singh <barandeepsingh@gmail.com>
2021-07-14 22:32:53 +0530

    Initial commit

---
+++ src/com/test/cheezeproblem/CheezeProblemInputDataMismatchException.java
@@ -0,0 +1,7 @@
+package com.test.cheezeproblem;
+
+public class CheezeProblemInputDataMismatchException extends RuntimeException {
+    public CheezeProblemInputDataMismatchException(String message) {
+        super(message);
+    }
+}
